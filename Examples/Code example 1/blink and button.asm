2329c3589c8589f1b15e10ccd68b8382c719bf47
Allan C. M. Lira <acmlira@gmail.com>
2018-08-14 22:21:17 -0300

    Missing Wiki (and need refactor) :construction:

--- Examples/Code example 1/blink and button.asm
+++ Examples/Code example 1/blink and button.asm
@@ -32,10 +32,11 @@
    #define      ctb1    bsf     STATUS, RP0                                     ;Cria mnemônico p/ mudar p/ Banco de Registradores 1
    
 ;  - Variáveis ou GPRs ------------------------------------------------------------------------------------------------------------------------------------------
-   cblock       H'000C'
+   cblock       H'0070'
    	
-   W_TEMP
-   STATUS_TEMP
+   W_TEMP                                                                       ;Variavel para salvamento de contexto
+   STATUS_TEMP                                                                  ;Variável para salvamento de contexto
+   COUNTER                                                                      ;Contador auxiliar para timer 0
    
    endc
 ;  - Vetor Reset ------------------------------------------------------------------------------------------------------------------------------------------------
@@ -56,28 +57,75 @@
 						ctb0 												    ;Muda para banco 0 para
 						movwf 	STATUS_TEMP 									;Salva STATUS no STATUS_TEMP
 						
-;  --- Rotinas de Interrupção -----------------------------------------------------------------------------------------------------------------------------------
+;  --- Chama rotinas de Interrupção -----------------------------------------------------------------------------------------------------------------------------
 
-;  	   ...						
+                        btfsc   INTCON, T0IF                                    ;Testa a flag de interrupção e pula uma linha se estiver zerada 
+                        call    ISR_T0                                          ;Chama rotina de interrupção
+                        btfsc   INTCON, INTF                                    ;Testa a flag de interrupção e pula uma linha se estiver zerada
+                        call    ISR_E					                        ;Chama rotina de interrupção
 						
 ;  -- Get Back Context ------------------------------------------------------------------------------------------------------------------------------------------						
-						
+
+ISR_exit:						
                         swapf   STATUS_TEMP, W                                  ;W = STATUS_TEMP(B'YYYY XXXX' -> B'XXXX YYYY') 
                         movwf   STATUS                                          ;STATUS = W (Pega STATUS original)
                         swapf   W_TEMP, F                                       ;W_TEMP = W_TEMP('ZZZZ WWWW' -> B'WWWW ZZZZ')
                         swapf   W_TEMP, W                                       ;W = W_TEMP(B'WWWW ZZZZ' -> B'ZZZZ WWWW')
 					
                         retfie                                                  ;Retorna de interrupção
+
+; - Interrupt Service Routine do Timer 0 ----------------------------------------------------------------------------------------------------------------------------------- 
+			
+ISR_T0:                 
+                        bcf     INTCON, T0IF                                    ;Limpa a flag de interrupção!
+                        decfsz  COUNTER, F
+                        goto    Aux
+                        call    VarRst
+                        ctb0                                                    ;Garante que estamos no Banco 0
+                        movlw   B'00100000'                                     ;Valor que vamos fazer xor (inverte o bit 5)
+                        xorwf   PORTB, F                                        ;PORTB = PORTB XOR W
+Aux:                    call    Timer0Rst                                       ;Recarrega timer 0
+                        goto    ISR_exit
+                                                
+; - Interrupt Service Routine do Externo ----------------------------------------------------------------------------------------------------------------------------------- 
+			
+ISR_E:                 
+                        bcf     INTCON, INTF                                    ;Limpa a flag de interrupção!
+                        ctb1                                                    ;Muda para Banco 1 vamos trabalhar com OPTION
+                        movlw   B'00000010'                                     ;Valor que vamos fazer xor (inverte o bit 1)
+                        xorwf   OPTION_REG, F                                   ;OPTION_REG = OPTION_REG XOR W
+                        ctb0                                                    ;Muda para Banco 0 (PADRÃO)
+						goto    ISR_exit
 						
 ; - Início do programa ------------------------------------------------------------------------------------------------------------------------------------------
 
 Start:					
-                        call    PinsRst
-                        call    InterruptionsRst
+                        call    VarRst                                          ;Inicializa variáveis
+                        call    Timer0Rst                                       ;Configura antes para não dar prob. quando habilitar interrupções
+                        call    PinsRst                                         ;Configura inicialmente os pinos
+                        call    InterruptionsRst                                ;Habilita as interrupções e o programa está pronto!
 Loop:					
-						
+                        nop                                                     ;Gasta 1 ciclo de máquina
+                        nop
+                        nop
                         goto    Loop                                            ;Fecha laço
-						
+
+; - 'Reseta' variáveis -----------------------------------------------------------------------------------------------------------------------------------------
+
+VarRst:                 
+                        movlw   D'10'                                           ;Valor que vamos carregar no contador auxiliar do timer 0                              
+                        movwf   COUNTER                                         ;COUNTER = W
+                        return                                                  ;Retorna para contexto do programa
+
+; - 'Reseta' timer 0 -------------------------------------------------------------------------------------------------------------------------------------------
+
+Timer0Rst:              
+                        ctb0                                                    ;Garante que estamos no Banco 0
+                        movlw   D'60'                                           ;W = D'60'
+                        movwf   TMR0                                            ;TMR0 conta de 60 a 255
+                        ctb0                                                    ;Redundância
+                        return                                                  ;Retorna para o contexto do programa
+                        						
 ; - 'Reseta' pinos ---------------------------------------------------------------------------------------------------------------------------------------------						
 
 PinsRst:				
@@ -104,7 +152,8 @@ InterruptionsRst:
                         movwf   INTCON                                          ;INTCON = W		    
                         ctb0                                                    ;Muda para Banco 0 por padrão do código
 					    return                                                  ;Retorna contexto para programa principal
-					    
+
 ; - Fim do programa --------------------------------------------------------------------------------------------------------------------------------------------		
+						
 						end														;Fim do programa
 												
