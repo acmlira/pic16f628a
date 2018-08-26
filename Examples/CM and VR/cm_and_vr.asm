3095a88b5a22b3e4cea6eac4479cc5d005bea0e3
Allan C. M. Lira <acmlira@gmail.com>
2018-08-25 21:31:30 -0300

    Adding new example: CM and VR :tada: :
    Comparator & Voltage Reference example :pray:

---
+++ Examples/CM and VR/cm_and_vr.asm
@@ -0,0 +1,122 @@
+;
+;    Autor: Allan César		License: CC-BY-4.0
+;
+;    Data: Agosto/2018
+;
+;    MCU Utilizada: PIC16F628A (Microchip)
+;
+;    Clock: ?? (XT)
+;  
+;    Projeto: ??
+;
+
+     list        p=16f628a
+
+;  - Arquivos incluídos -----------------------------------------------------------------------------------------------------------------------------------------
+   
+     #include    <p16f628a.inc>
+   
+;  - FUSE bits --------------------------------------------------------------------------------------------------------------------------------------------------
+;  
+;    Configura oscilador externo p/ RA6 e RA7, desliga Watchdog Timer, liga Power-Up Timer, liga a função Reset do pino RA5, desliga BOD, LVP desligado, Data e
+;    Code Protection desligado
+;
+;    Portanto RA5, RA6, RA7 -> XXX ou HIGH-Z (input)
+;
+
+     __config    _XT_OSC & _WDT_OFF & _PWRTE_ON & _MCLRE_ON & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF
+
+;  - Paginação de memória ---------------------------------------------------------------------------------------------------------------------------------------
+;
+;    Aqui em função do espelhemento de alguns SFR nos outros bancos (2 e 3) somente essa paginação é necessária
+;
+
+     #define     ctb0    bcf     STATUS, RP0                                     ;Cria mnemônico p/ mudar p/ Banco de Registradores 0
+     #define     ctb1    bsf     STATUS, RP0                                     ;Cria mnemônico p/ mudar p/ Banco de Registradores 1
+   
+;  - Variáveis ou GPRs ------------------------------------------------------------------------------------------------------------------------------------------
+     
+     cblock      H'0070'                                                         ;Inicia alocação de memória no endereço 70h
+   	    
+     W_TEMP                                                                      ;Auxiliar para guardar W (acumulador) antes de interrupção
+     STATUS_TEMP                                                                 ;Auxiliar para guardar STATUS antes interrupção
+   
+     endc                                                                        ;Termina alocação de memória
+     
+;  - Vetor Reset ------------------------------------------------------------------------------------------------------------------------------------------------
+
+                        org     H'0000'                                          ;Endereço de origem de todo programa
+                        goto    Start                                            ;Desvia do vetor de interrupção
+
+;  - Vetor de Interrupção ---------------------------------------------------------------------------------------------------------------------------------------
+
+                        org     H'0004'                                          ;Todas as interrupções apontam para este endereço
+
+;  --- Context Saving -------------------------------------------------------------------------------------------------------------------------------------------						
+;
+;      Salva contexto antes de ir para rotinas de interrupção e usa SWAP para não ter uma flag Z no STATUS do contexto
+;						
+						
+                        movwf   W_TEMP                                           ;W_TEMP = W(B'ZZZZ WWWW')
+                        swapf   STATUS,W                                         ;W = STATUS(B'XXXX YYYY' -> B'YYYY XXXX') 
+                        ctb0                                                     ;Muda para banco 0 para
+                        movwf   STATUS_TEMP                                      ;Salva STATUS no STATUS_TEMP
+						
+;  ----- Rotinas de Interrupção ---------------------------------------------------------------------------------------------------------------------------------
+
+                        bcf     PIR1, CMIF
+                        bsf     PORTB, 7
+                        btfss   CMCON, C2OUT
+                        bcf     PORTB, 7
+                        goto    ISR_Exit			
+						
+;  --- Get Back Context -----------------------------------------------------------------------------------------------------------------------------------------						
+						
+ISR_Exit:
+                        swapf   STATUS_TEMP, W                                   ;W = STATUS_TEMP(B'YYYY XXXX' -> B'XXXX YYYY') 
+                        movwf   STATUS                                           ;STATUS = W (Pega STATUS original)
+                        swapf   W_TEMP, F                                        ;W_TEMP = W_TEMP('ZZZZ WWWW' -> B'WWWW ZZZZ')
+                        swapf   W_TEMP, W                                        ;W = W_TEMP(B'WWWW ZZZZ' -> B'ZZZZ WWWW')
+                        retfie                                                   ;Retorna de interrupção
+						
+; - Início do programa ------------------------------------------------------------------------------------------------------------------------------------------
+
+Start:					
+                        ctb1
+                        bsf     TRISA, RA1
+                        bsf     TRISA, RA2
+                        bcf     TRISB, RB7
+                        movlw   B'10001000'
+                        movwf   VRCON
+                        movlw   B'01000000'
+                        movwf   PIE1
+                        
+                        ctb0
+                        movlw   B'00101010'
+                        movwf   CMCON
+                        
+                        movlw   B'11000000'
+                        movwf   INTCON
+;                       ...
+
+; - Rotina de loop para trabalhos contínuos ---------------------------------------------------------------------------------------------------------------------
+
+Loop:					
+
+;                       ...
+		
+                        goto    Loop                                             ;Fecha laço
+
+; - Reset do modulo comparador ----------------------------------------------------------------------------------------------------------------------------------
+
+Reset_Comparator:                        
+                        ctb0                                                     ;Muda para banco 0 p/ trabalhar com CMCON
+                        movlw   H'0007'                                          ;Desabilita CMCON
+                        movwf   CMCON                                            ;CMCON = W
+                        return                                                   ;Retorna contextualmente para o programa
+                        
+                        
+                        
+                        
+                        						
+                        end                                                      ;Fim do programa
